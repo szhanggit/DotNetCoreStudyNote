85e00fa9b80e15578bdf7880d3ed8dff41d9b85b
szhanggit <zhangyidingchina@hotmail.com>
2026-06-11 01:03:16 -0400

    asdfasdf

---
+++ Middleware/FluentValidation/FluentValidationCore31/FluentValidationCore31/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidationCore31.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            PersonModel person = new PersonModel() { FirstName = "Dave", LastName = "Davis" };
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]PersonModel person)
+        {
+            return Ok($"{person.FirstName} {person.LastName}");
+        }
+    }
+}
---
+++ Middleware/FluentValidation/FluentValidationCore31/FluentValidationCore31/PersonModel.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationCore31
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class PersonModelValidator : AbstractValidator<PersonModel>
+    {
+        public PersonModelValidator()
+        {
+            RuleFor(p => p.FirstName).NotEmpty();
+            RuleFor(p => p.LastName).Length(5);
+        }
+    }
+}
