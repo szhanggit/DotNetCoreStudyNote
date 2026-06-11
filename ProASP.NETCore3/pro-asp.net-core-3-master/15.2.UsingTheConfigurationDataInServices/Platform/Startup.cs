656832c8af3009d5f28bd256f32755e961382fe9
szhanggit <zhangyidingchina@hotmail.com>
2026-06-11 01:24:41 -0400

    sdf

---
+++ ProASP.NETCore3/pro-asp.net-core-3-master/15.2.UsingTheConfigurationDataInServices/Platform/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Platform
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configService)
+        {
+            Configuration = configService;
+        }
+        private IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.Use(async (context, next) => {
+                string defaultDebug = Configuration["Logging:LogLevel:Default"];
+                await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            //http://localhost:5000/
+        }
+    }
+}
---
+++ ProASP.NETCore3/pro-asp.net-core-3-master/15.6.StoringUserSecrets/Platform/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Platform
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configService)
+        {
+            Configuration = configService;
+        }
+        private IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<MessageOptions>(Configuration.GetSection("Location"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseMiddleware<LocationMiddleware>();
+
+            app.Use(async (context, next) => {
+                string defaultDebug = Configuration["Logging:LogLevel:Default"];
+                await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
+                string environ = Configuration["ASPNETCORE_ENVIRONMENT"];
+                await context.Response.WriteAsync($"\nThe env setting is: {environ}");
+                string wsID = Configuration["WebService:Id"];
+                string wsKey = Configuration["WebService:Key"];
+                await context.Response.WriteAsync($"\nThe secret ID is: {wsID}");
+                await context.Response.WriteAsync($"\nThe secret Key is: {wsKey}");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            //http://localhost:5000
+        }
+    }
+}
